f962100cade52f5e8ec4659089ea788d5ee16783
Jonathan Dickert <jon.p.dickert@gmail.com>
2021-01-27 20:21:49 -0500

    have all desired libs for now

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,4 @@
-#include <GLFW/glfw3.h>
-#include <sol/sol.hpp>
-
-#include <iostream>
-
 int main(int, char*[])
 {
-  sol::state state;
-  state.script("foo = 1;");
-  std::cout << state.get<int>("foo") << '\n';
-
   return 0;
 }
