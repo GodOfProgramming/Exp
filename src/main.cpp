9db0a7a1dc33eaf2ff426c2c8d91a2e71a732ea5
Jonathan Dickert <jon.p.dickert@gmail.com>
2021-01-27 19:49:11 -0500

    compiles with libs

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <GLFW/glfw3.h>
 #include <sol/sol.hpp>
 
 #include <iostream>
@@ -7,5 +8,6 @@ int main(int, char*[])
   sol::state state;
   state.script("foo = 1;");
   std::cout << state.get<int>("foo") << '\n';
+
   return 0;
 }
