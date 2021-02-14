8ad70da29ecfc59a46e612e48456bb95b3ef0d8c
Jonathan Dickert <jon.p.dickert@gmail.com>
2021-02-14 17:46:41 -0500

    better square

--- assets/scripts/game/square.lua
+++ assets/scripts/game/square.lua
@@ -3,7 +3,7 @@ function Instantiate(self)
 end
 
 function Update(self)
-  local green_value = math.sin(GameInfo.frames) / 2.0 + 0.5
+  local green_value = math.sin(GameInfo.frames * 0.1) / 2.0 + 0.5
   local uniform = self.uniforms:get("color");
   uniform:set_vec4(0.0, green_value, 0.0, 1.0);
 end
