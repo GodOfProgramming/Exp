9f5c641bbeae4c784a7978871ba527538936db50
Jonathan Dickert <jon.p.dickert@gmail.com>
2021-02-24 19:13:08 -0500

    removed globals in square lua script

--- assets/scripts/src/game/square.lua
+++ assets/scripts/src/game/square.lua
@@ -5,29 +5,30 @@ game = require('game');
 info = game.info.instance();
 camera = game.camera.instance();
 
-u_colors = nil;
-u_model = nil;
-u_view = nil;
-u_proj = nil
+function construct(obj)
+  local u_colors = gl.uniform.create("u_colors");
+  obj.uniforms:set("colors", u_colors);
 
-self = nil;
+  local u_model = gl.uniform.create("u_model");
+  obj.uniforms:set("model", u_model);
 
-function construct(square)
-  self = square;
-  u_colors = gl.uniform.create("u_colors");
-  self.uniforms:set("colors", u_colors);
+  local u_view = gl.uniform.create("u_view");
+  obj.uniforms:set("view", u_view);
 
-  u_model = gl.uniform.create("u_model");
-  self.uniforms:set("model", u_model);
+  local u_proj = gl.uniform.create("u_projection");
+  obj.uniforms:set("projection", u_proj);
 
-  u_view = gl.uniform.create("u_view");
-  self.uniforms:set("view", u_view);
-
-  u_proj = gl.uniform.create("u_projection");
-  self.uniforms:set("projection", u_proj);
+  obj.data = {
+    uniforms = {
+      colors = u_colors,
+      model = u_model,
+      view = u_view,
+      proj = u_proj,
+    }
+  };
 end
 
-function update(_)
+function update(obj)
   if info.state ~= game.state.PLAYING then
     return;
   end
@@ -45,14 +46,16 @@ function update(_)
     geom.vec3.new(rgb, rgb, rgb),
   };
 
-  u_colors:set_v_vec3(colors);
+  local uniforms = obj.data.uniforms;
+
+  uniforms.colors:set_v_vec3(colors);
 
   local size = 400;
 
   local transform = geom.mat4.identity();
   transform = transform:translate(geom.vec3.new(size, size / 2, 0));
   transform = transform:scale(geom.vec3.new(size, size, 0));
-  u_model:set_mat4(transform);
-  u_view:set_mat4(camera:get_view());
-  u_proj:set_mat4(camera:get_projection());
+  uniforms.model:set_mat4(transform);
+  uniforms.view:set_mat4(camera:get_view());
+  uniforms.proj:set_mat4(camera:get_projection());
 end
